f2c803a54b216950c25c8527160d79dca449a37c
Aman Jain <amanjain9205@gmail.com>
2026-08-15 19:59:28 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("Namaste Node.js");
+
+const express = require("express");
+const app = express();
+
+app.use("/test",(req, res) => {
+ res.send("Hello from the server!!");
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
