ea043e93597fdac957402e7a2b46d6a95d879bf9
Aman Jain <amanjain9205@gmail.com>
2026-08-17 11:35:25 +0530

    Refactor user route handling and error middleware

--- src/app.js
+++ src/app.js
@@ -2,25 +2,15 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-
-const {adminAuth} = require("./middlewares/adminAuth");
-const {userAuth} = require("./middlewares/userAuth");
-
-app.use('/admin', adminAuth, (req, res, next) => {
-  res.send("Admin auth route");
+app.use('/user',  (req, res, next) => {
+  // res.send("User routes");
+  throw new Error("User route error");
 });
 
-app.get('/admin/getAllData', adminAuth, (req, res) => {
-  res.send("Admin route authenticated all data");
-} );
-
-app.use('/user', userAuth,  (req, res, next) => {
-  // res.send("User routes");
-  next();
-},
-(req, res) => {
-  res.send("User routes 2");
-} );
+app.use("/", (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 app.listen(7777, () => {
  console.log("Server is successfully listening on port 7777");
